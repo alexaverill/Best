78842f897cedb5a65b493f35c191dec1c41023af
Alex Averill <alex.averill2013@gmail.com>
2014-11-17 20:46:24 -0600

    Adding templating.

--- header.php
+++ header.php
@@ -3,13 +3,19 @@
 error_reporting(0);
 include('session.php');
 include('database.php');
+require_once './Twig/lib/Twig/Autoloader.php';
+Twig_Autoloader::register();
+$loader = new Twig_Loader_Filesystem('./templates');
+$twig = new Twig_Environment($loader, array());
+    
 if (version_compare(phpversion(), '5.5.0', '<')) {
     //If the php Version is not supporting password hash, lets include it
-    require('source/lib/password.php');
+    //require('source/lib/password.php');
 }
 try{
     $dbh= new PDO('mysql:host='.$data_host.';dbname='.$name_database,$data_username,$data_password);
 }catch(PDOException $e){
-    echo $e->getMessage();
+    //echo $e->getMessage();
 }
+include('templates/head.html');
 ?>
